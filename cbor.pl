6691b153fe22e151a79c7c3914f38d0f7d9cd98f
Shikiyo <shikiyo@archjk>
2025-01-27 18:23:40 -0300

    Remove constrain #X #= \ #V from negative

--- cbor.pl
+++ cbor.pl
@@ -163,7 +163,7 @@ cbor_0_value_x(reserved(29), nwf(29)) --> [].
 cbor_0_value_x(reserved(30), nwf(30)) --> [].
 cbor_0_value_x(indefinite, nwf(31)) --> [].
 
-cbor_1_value_x(val(P, V), negative(P, X)) --> { #X #< 0, #X #= \ #V, #X + #V #= -1 }.
+cbor_1_value_x(val(P, V), negative(P, X)) --> { #X #< 0, #X + #V #= -1 }.
 % Not well-formed: 0x20 + 28 = 60
 cbor_1_value_x(reserved(28), nwf(60)) --> [].
 cbor_1_value_x(reserved(29), nwf(61)) --> [].
